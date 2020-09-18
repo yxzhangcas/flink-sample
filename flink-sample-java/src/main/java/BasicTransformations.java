9d477094696a764b7dfa746f475a9a9f7f354571
zhangyxd <zhangyxd@sugon.com>
2020-09-18 15:58:47 +0800

    添加样例代码

---
+++ flink-sample-java/src/main/java/BasicTransformations.java
@@ -0,0 +1,47 @@
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.streaming.api.TimeCharacteristic;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import udf.IdExtractor;
+import udf.IdSplitter;
+import udf.TemperatureFilter;
+import util.SensorReading;
+import util.SensorSource;
+import util.SensorTimeAssigner;
+
+public class BasicTransformations {
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
+        env.getConfig().setAutoWatermarkInterval(1000L);
+
+        DataStream<SensorReading> readings = env
+                .addSource(new SensorSource())
+                .assignTimestampsAndWatermarks(new SensorTimeAssigner());
+
+        DataStream<SensorReading> filteredReadings = readings
+                .filter(new TemperatureFilter(25));
+                //.filter(r -> r.temperature >= 25);
+
+        DataStream<String> sensorIds = readings
+                .map(new IdExtractor());
+                //.map(r -> r.id);
+
+        DataStream<String> splitIds = sensorIds
+                .flatMap(new IdSplitter());
+                /*
+                //需要添加类型声明，避免Lambda的歧义
+                .flatMap((FlatMapFunction<String, String>)(id, out) -> {
+                    for (String s : id.split("_")) {
+                        out.collect(s);
+                    }
+                })
+                //Java语言是静态类型，需要明确指定Lambda的返回值类型
+                .returns(Types.STRING);
+                 */
+
+        splitIds.print();
+        env.execute();
+    }
+}

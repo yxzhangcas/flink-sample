7d7ed9c225e70ba2ae67aad9a197eaea8e5ceec6
zhangyxd <zhangyxd@sugon.com>
2020-09-18 17:45:27 +0800

    添加样例代码

---
+++ flink-sample-scala/src/main/scala/RollingSum.scala
@@ -0,0 +1,12 @@
+import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
+import org.apache.flink.streaming.api.scala._
+
+object RollingSum {
+  def main(args: Array[String]): Unit = {
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+    val inputStream = env.fromElements((1,2,2),(2,3,1),(2,2,4),(1,5,3))
+    val resultStream = inputStream.keyBy(0).sum(1)
+    resultStream.print()
+    env.execute()
+  }
+}

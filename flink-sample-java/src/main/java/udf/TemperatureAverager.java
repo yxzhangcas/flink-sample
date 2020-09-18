86a687c8def8ad4c89d23a81f74e92faaa25f8e4
zhangyxd <zhangyxd@sugon.com>
2020-09-18 12:43:10 +0800

    添加Java样例

---
+++ flink-sample-java/src/main/java/udf/TemperatureAverager.java
@@ -0,0 +1,20 @@
+package udf;
+
+import org.apache.flink.streaming.api.functions.windowing.WindowFunction;
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
+import org.apache.flink.util.Collector;
+import util.SensorReading;
+
+public class TemperatureAverager implements WindowFunction<SensorReading, SensorReading, String, TimeWindow> {
+    @Override
+    public void apply(String s, TimeWindow timeWindow, Iterable<SensorReading> iterable, Collector<SensorReading> collector) throws Exception {
+        int cnt = 0;
+        double sum = 0.0;
+        for (SensorReading r : iterable) {
+            cnt++;
+            sum += r.temperature;
+        }
+        double avgTemp = sum / cnt;
+        collector.collect(new SensorReading(s, timeWindow.getEnd(), avgTemp));
+    }
+}
---
+++ flink-sample-java/src/main/java/util/SensorTimeAssigner.java
@@ -0,0 +1,14 @@
+package util;
+
+import org.apache.flink.streaming.api.functions.timestamps.BoundedOutOfOrdernessTimestampExtractor;
+import org.apache.flink.streaming.api.windowing.time.Time;
+
+public class SensorTimeAssigner extends BoundedOutOfOrdernessTimestampExtractor<SensorReading> {
+    public SensorTimeAssigner() {
+        super(Time.seconds(5));
+    }
+    @Override
+    public long extractTimestamp(SensorReading sensorReading) {
+        return sensorReading.timestamp;
+    }
+}
